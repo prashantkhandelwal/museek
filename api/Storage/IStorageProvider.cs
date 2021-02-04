d70b00e94d6a3d041e254c4822aa040736743c55
Prashant Khandelwal <prashant@midnightprogrammer.net>
2021-02-04 22:02:48 +0530

    Add changes for DB access.

---
+++ api/Storage/IStorageProvider.cs
@@ -0,0 +1,17 @@
+﻿using api.Models;
+using MongoDB.Driver.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Storage
+{
+    /// <summary>
+    /// Provide the implementation for multiple datasources.
+    /// </summary>
+    public interface IStorageProvider<T> where T : class, new()
+    {
+        IMongoQueryable<T> Query {get; set;}
+    }
+}
---
+++ api/Storage/Providers/MongoDBProvider.cs
@@ -0,0 +1,48 @@
+﻿using api.Models;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace api.Storage.Providers
+{
+    public class MongoDBProvider<T> : IStorageProvider<T> where T: class, new()
+    {
+        private readonly IMongoClient _client;
+        private readonly IMongoDatabase _database;
+        private readonly IStorageSettings _settings;
+        private string _collection;
+        
+        public MongoDBProvider(IStorageSettings settings)
+        {
+            _client = new MongoClient(_settings.Host);
+            _database = _client.GetDatabase(_settings.Database);
+        }
+
+        protected IMongoCollection<T> Collection
+        {
+            get
+            {
+                return _database.GetCollection<T>(_collection);
+            }
+            set
+            {
+                Collection = value;
+            }
+        }
+
+        public IMongoQueryable<T> Query 
+        {
+            get => Collection.AsQueryable<T>();  
+            set => Query = value; 
+        }
+
+        public T Get(Expression<Func<T, bool>> expression)
+        {
+            return Collection.Find(expression).SingleOrDefault();
+        }
+    }
+}

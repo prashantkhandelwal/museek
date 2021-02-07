3a835314d100c9349f1f8dccd61fd567ede3d622
Prashant Khandelwal <prashant@midnightprogrammer.net>
2021-02-07 21:59:31 +0530

    Exact search for the artist name.

--- api/Storage/DB.cs
+++ api/Storage/DB.cs
@@ -21,15 +21,38 @@ namespace api.Storage
             _collection = _database.GetCollection<Artist>("artist");
         }
 
+        /// <summary>
+        /// Gets the artist on the basis of the artist name on the basis of
+        /// case sensitivity and exact match of the name.
+        /// </summary>
+        /// <param name="SearchText"></param>
+        /// <param name="IsMatch"></param>
+        /// <returns>Artist</returns>
         public IEnumerable<dynamic> Get(string SearchText, bool IsMatch)
         {
             TextSearchOptions options = new TextSearchOptions
             {
-                CaseSensitive = IsMatch ? true: false,
+                CaseSensitive = false,
                 DiacriticSensitive = true
             };
 
             FilterDefinition<Artist> filter = Builders<Artist>.Filter.Text(SearchText, options);
+
+            if (IsMatch)
+            {
+                return _collection.Find(x => x.Name == SearchText).Project(p =>
+              new
+              {
+                  Name = p.Name,
+                  Id = p.Id,
+                  Country = p.Country,
+                  Genres = p.Genres.Select(g => g.Name),
+                  Rating = p.Rating,
+                  KnownAlias = p.KnownAlias.Select(a => a.Name),
+                  LifeSpan = p.LifeSpan,
+              }).ToList();
+            }
+
             return _collection.Find(filter).Project(p =>
             new
             {

f738613800a459927911cc86894b9b8684b2aa22
Prashant Khandelwal <prashant@midnightprogrammer.net>
2021-02-09 20:32:41 +0530

    Integrate material-ui. Other UI changes.

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,19 +1,40 @@
+import { AppBar, Button, Container, IconButton, TextField, Toolbar, Typography } from '@material-ui/core';
+import { useState } from 'react';
+
 function App() {
-  
-  const call = async () => {
-    const res = await fetch("https://localhost:44387/brainz?name=eminem")
-    const data = res.json();
-    console.log(data);
+
+  const [artistName, setArtistName] = useState('');
+
+  const search = async () => {
+    const res = await fetch("https://localhost:44387/brainz?name=" + artistName);
+    const data = res.json().then(function(d){
+      console.log(d.results[0].name);
+    })
+    
     return data;
   }
-  
+
   return (
-    <div className="App">
-      <header className="App-header">
-       <input type="text"/>
-       <button onClick={call}>Search</button>
-      </header>
-    </div>
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+          </IconButton>
+          <Typography variant="h6">
+            Museek
+    </Typography>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="sm" style={{ paddingTop: "40px" }}>
+        <div className="App">
+          <header className="App-header">
+            <TextField id="standard-basic" label="Artist Name" value={artistName}
+              onChange={(e) => setArtistName(e.target.value)} />
+            <Button style={{ marginTop: "13px", marginLeft: "8px" }} variant="contained" color='primary' onClick={search}>Search</Button>
+          </header>
+        </div>
+      </Container>
+    </>
   );
 }
 

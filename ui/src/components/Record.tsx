9b533020b1883856e00e68aebec5da960e5d4dd3
prashantkhandelwal <prashant@midnightprogrammer.net>
2023-03-14 21:14:17 +0530

    Update package.json - removed styled-components.

--- ui/src/components/Record.tsx
+++ ui/src/components/Record.tsx
@@ -9,33 +9,36 @@ interface IRecord {
 export const Record: React.FC<IRecord> = (props: IRecord) => {
     return (
         <Wrapper>
-            <Box width="350px" paddingBottom={0}>
-                <Card>
-                    <CardContent>
-                        <Typography variant="inherit" align="left" component="div">
-                            <span className="rkeys">Song Name:</span>
-                            <span className="rvalues">{props.data.title}</span>
-                        </Typography>
-                        <Typography variant="inherit" align="left" component="div">
-                            <span className="rkeys">Duration:</span>
-                            <span className="rvalues">{props.data.length}</span>
-                        </Typography>
-                        <Typography variant="inherit" align="left" component="div">
-                            <span className="rkeys">Release Date:</span>
-                            <span className="rvalues">{props.data["first-release-date"]}</span>
-                        </Typography>
-                        <Typography variant="inherit" align="left" component="div">
-                            <span className="rkeys">Genres:</span> {
-                                props.data.genres.map((g) => (
-                                    <span className="rvalues">{g.name},</span>
-                                ))}
-                        </Typography>
-                        <Typography variant="inherit" align="left" component="div">
-                            <Rating name="read-only" value={props.data.rating.value} readOnly />
-                        </Typography>
-                    </CardContent>
-                </Card>
-            </Box>
-        </Wrapper>
+            {(!props.data.video) ?
+                <Box width="350px" paddingBottom={0}>
+                    <Card>
+                        <CardContent>
+                            <Typography variant="inherit" align="left" component="div">
+                                <span className="rkeys">Song Name:</span>
+                                <span className="rvalues">{props.data.title}</span>
+                            </Typography>
+                            <Typography variant="inherit" align="left" component="div">
+                                <span className="rkeys">Duration:</span>
+                                <span className="rvalues">{props.data.length}</span>
+                            </Typography>
+                            <Typography variant="inherit" align="left" component="div">
+                                <span className="rkeys">Release Date:</span>
+                                <span className="rvalues">{props.data["first-release-date"]}</span>
+                            </Typography>
+                            <Typography variant="inherit" align="left" component="div">
+                                <span className="rkeys">Genres:</span> {
+                                    props.data.genres.map((g) => (
+                                        <span className="rvalues">{g.name},</span>
+                                    ))}
+                            </Typography>
+                            <Typography variant="inherit" align="left" component="div">
+                                <Rating name="read-only" value={props.data.rating.value} readOnly />
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Box>
+                : <></>
+            }
+        </Wrapper >
     )
 }
